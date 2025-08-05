a68219eb68ef104ddb10ee45ed65a557c866d559
zosein <italoledz@gmail.com>
2025-08-04 23:45:30 -0400

    estudos poo c#

---
+++ 01 Heranca/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("*** POO - Herança ***");
+
+Funcionario func = new();
+func.Nome = "José";
+func.Email = "jose@email.com";
+func.Empresa = "Amazon";
+func.Salario = 9000;
+
+Console.WriteLine("Funcionário: ");
+Console.WriteLine(func.Empresa);
+Console.WriteLine(func.Salario);
+func.Identificar();
+
+Aluno alumni = new Aluno();
+alumni.Nome = "Roberto";
+alumni.Email = "roberto@alumni.com";
+alumni.Curso = "Arquitetura";
+alumni.Nota = 9;
+
+Console.WriteLine("Aluno: ");
+Console.WriteLine(alumni.Curso);
+Console.WriteLine(alumni.Nota);
+alumni.Identificar();
+
+
+Console.ReadKey();
+
+public class Pessoa
+{
+    public string? Nome { get; set; }
+    public string? Email { get; set; }
+    public void Identificar()
+    {
+        Console.WriteLine($"{Nome} - {Email}");
+    }
+}
+
+public class Funcionario : Pessoa
+{
+    public string? Empresa { get; set; }
+    public decimal Salario { get; set; }
+}
+
+public class Aluno : Pessoa
+{
+    public string? Curso { get; set; }
+    public int Nota { get; set; }
+}
---
+++ 03 Heranca_Construtores/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("*** Herança - Construtores ***");
+// a classe base e a classe derivada podem possuir seus próprios construtores
+// a classe derivada não herda o construtor da classe base mas pode invoca-lo
+//Ao criar uma instância da classe derivada o construtor da classe base sem parâmetros
+//será invocado primeiro e depois será invocado o construtor da classe derivada
+
+Aluno aluno1 = new Aluno();
+Aluno aluno2 = new Aluno("José");
+
+
+Console.ReadKey();
+
+//classe base
+class Pessoa
+{
+    public Pessoa()
+    {
+        Console.WriteLine("Construtor da classe Pessoa");
+    }
+    public Pessoa(string nome)
+    {
+        Console.WriteLine("Construtor da classe Pessoa com parametro");
+    }
+}
+
+//classe derivada
+class Aluno : Pessoa
+{
+    public Aluno() : base()
+    {
+        Console.WriteLine("Construtor da classe Aluno");
+    }
+    public Aluno(string nome) : base(nome)
+    {
+        Console.WriteLine("Construtor da classe Aluno com parametro");
+    }
+}
