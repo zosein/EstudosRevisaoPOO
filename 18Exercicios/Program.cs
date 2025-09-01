266eabe9b3d2e0e1d6f60f620c446f50d1d1f70c
zosein <italoledz@gmail.com>
2025-08-31 23:57:38 -0400

    exercicios revisão poo c#

---
+++ 18Exercicios/Program.cs
@@ -0,0 +1,9 @@
+﻿using _18Exercicios;
+
+Carro carro = new Carro(0);
+Console.WriteLine("Informe quantos litros de gasolina: ");
+int gasolina = Convert.ToInt32(Console.ReadLine());
+if (carro.Abastecer(gasolina))
+{
+    carro.Dirigir();
+}
